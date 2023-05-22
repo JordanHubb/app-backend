cd51e7aca5c576014501ffe26f86375ce4600fb6
JordanHubb <Fenstersteuerungber SupportAhmad.Matar@Student.HTW-Berlin.de>
2023-05-22 10:58:28 +0200

    update

--- src/main/java/de/HTWBerlin/webtech/Webtech/WebtechApplication.java
+++ src/main/java/de/HTWBerlin/webtech/Webtech/WebtechApplication.java
@@ -2,14 +2,10 @@ package de.HTWBerlin.webtech.Webtech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
-
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = DataSourceAutoConfiguration.class))
 
 
+
+@SpringBootApplication
 public class WebtechApplication {
 
 	public static void main(String[] args) {
